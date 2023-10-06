4072ef78944870e74c4129b944919f12a54ad3cd
Anillc <i@anillc.cn>
2023-10-06 18:35:33 +0800

    update scripts

--- flake.nix
+++ flake.nix
@@ -6,14 +6,50 @@
     devshell.url = "github:numtide/devshell";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs@{ flake-parts, ... }: let
+    in flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ inputs.devshell.flakeModule ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, pkgs, system, ... }: {
+      perSystem = { config, pkgs, lib, system, ... }: let
+        # =======================================================================
+        # configuration
+        abi = map toString [ 64 67 72 79 83 102 108 111 115 ];
+        os = [ "linux-glibc-x64" "darwin-unknown-x64" "win32-unknown-x64" ];
+        env = rec {
+          NAME = "canvas";
+          REPO = "Automattic/node-canvas";
+          VERSION = "v2.11.2";
+          NODE_ABI_VERSIONS = lib.concatStringsSep " " abi;
+          NODE_OS_LIST = lib.concatStringsSep " " os;
+          FOLDERS = let
+            set = lib.cartesianProductOfSets { inherit abi os; };
+            list = map ({ abi, os }: "${NAME}-${VERSION}-node-v${abi}-${os}") set;
+          in lib.concatStringsSep " " list;
+        };
+        # =======================================================================
+      in {
         devshells.default = {
-          env = [{ name = "NPM_TOKEN"; eval = "$(cat ~/.config/npm-token/NPM_TOKEN)"; }];
+          env = [
+            { name = "NPM_TOKEN"; eval = "$(cat ~/.config/npm-token/NPM_TOKEN)"; }
+          ] ++ lib.mapAttrsToList (name: value: {
+            inherit name value;
+          }) env;
           packages = with pkgs; [ gnutar unzip curl yarn ];
+          commands = [{
+            name = "init-subtree";
+            help = "init subtree ${env.NAME}";
+            command = "git subtree --prefix=canvas add https://github.com/${env.REPO}.git ${env.VERSION} --squash";
+          } {
+            name = "pull-subtree";
+            help = "pull ${env.NAME} with tag ${env.VERSION}";
+            command = "git subtree --prefix=canvas pull ${env.VERSION} --squash";
+          } {
+            name = "build-nereid";
+            command = "cd nereid && ./build.sh";
+          } {
+            name = "pub-nereid";
+            command = "cd nereid && ./pub.sh";
+          }];
         };
       };
     };
--- nereid/build.sh
+++ nereid/build.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
-set -eu
-
-REPO="Automattic/node-canvas"
-NAME="canvas"
-VERSION="v2.11.2"
-NODE_ABI_VERSIONS="64 67 72 79 83 102 108 111 115"
-OS="linux-glibc-x64 darwin-unknown-x64 win32-unknown-x64"
+set -e
 
 rm -rf canvas-*
-for abi in $NODE_ABI_VERSIONS; do
-  for os in $OS; do
-    FILE=$(mktemp)
-    FOLDER=$NAME-$VERSION-node-v$abi-$os
-    URL=https://github.com/$REPO/releases/download/$VERSION/$NAME-$VERSION-node-v$abi-$os.tar.gz
-    echo $URL
-    curl -o $FILE -L $URL
-    mkdir $FOLDER
-    tar xzvf $FILE -C $FOLDER
-    rm $FILE
-    yarn nereid-cli build $FOLDER
-  done
+for FOLDER in $FOLDERS; do
+  FILE=$(mktemp)
+  curl -o $FILE -L https://github.com/$REPO/releases/download/$VERSION/$FOLDER.tar.gz
+  mkdir $FOLDER && tar xzvf $FILE -C $FOLDER
+  rm $FILE
+  yarn nereid-cli build $FOLDER
 done
