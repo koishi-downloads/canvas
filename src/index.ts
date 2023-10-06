6f1645a8ab022cec81fca3d418e4dd3d41df8506
Anillc <i@anillc.cn>
2023-10-06 22:20:34 +0800

    setup downloads

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { platform } from 'os'
 import { Context, Schema } from 'koishi'
 import registry from 'get-registry'
-import {} from 'koishi-plugin-downloads'
+import { NereidTask } from 'koishi-plugin-downloads'
 import {} from 'koishi-plugin-nix'
+import { version } from './dep/package.json'
 
 export interface Config {
   nix: boolean
@@ -15,8 +17,34 @@ export const name = 'canvas'
 export const using = ['downloads']
 
 export async function apply(ctx: Context) {
-  // const task = ctx.downloads.nereid('ffmpeg', [
-  //   `npm://@koishijs-assets/canvas?registry=${await registry()}`
-  // ], bucket())
-  
+  const task = ctx.downloads.nereid(name, [
+    `npm://@koishijs-assets/${name}?registry=${await registry()}`
+  ], bucket())
+  if (ctx.config.nix) {
+    ctx.using(['nix'], ctx => plugin(ctx, task))
+  } else {
+    plugin(ctx, task)
+  }
+}
+
+async function plugin(ctx: Context, task: NereidTask) {
+  await task.promise
+}
+
+export function bucket() {
+  let os: string
+  switch (platform()) {
+    case 'linux':
+      os = 'linux-glibc-x64'
+      break
+    case 'darwin':
+      os = 'darwin-unknown-x64'
+      break
+    case 'win32':
+      os = 'win32-unknown-x64'
+      break
+    default:
+      throw new Error('unsupported platform')
+  }
+  return `${name}-v${version}-node-v${process.versions.modules}-${os}`
 }
